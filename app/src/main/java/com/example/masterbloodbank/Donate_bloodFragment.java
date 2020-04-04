e907d34e2a99d1396fc00d681daf480546ff688b
jishnu234 <jishnuvedhikz88@gmail.com>
2020-04-04 09:41:42 +0530

    second commit

---
+++ app/src/main/java/com/example/masterbloodbank/Donate_bloodFragment.java
@@ -0,0 +1,28 @@
+package com.example.masterbloodbank;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Donate_bloodFragment extends Fragment {
+
+    public Donate_bloodFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donate_blood, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/masterbloodbank/needMenuFragment.java
@@ -0,0 +1,24 @@
+package com.example.masterbloodbank;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class needMenuFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_need_menu, container, false);
+    }
+}
